f7c6b066dd5bbd326afbcfacc10064658ee3ad66
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-08-16 10:25:29 -0400

    hello world

---
+++ practice/hello.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.util.function.Function;
+
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello CCCS315");        
+    }
+}
